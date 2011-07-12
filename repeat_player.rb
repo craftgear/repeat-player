7f236218b67127d14bb3d95842f726945d9a8811
Shunsuke Watanabe <craftgear@gmail.com>
2011-07-12 16:47:37 +0900

    add ensure

--- repeat_player.rb
+++ repeat_player.rb
@@ -3,7 +3,7 @@ require "colored"
 
 #設定
 @ini_file = "repeat_player.ini"
-@default_speeds = [1.8, 1.2, 1.5]
+@default_speeds = [1.8, 1.2, 1.4]
 #default_speeds = [2.0, 1.6, 1.2]
 #default_speeds = [1.7, 1.4, 1.2, 1.0]
 file_types = "mp4,avi,mpg,mkv,mp3,flac,m4a"
@@ -21,9 +21,18 @@ def load_ini
           last_file = line.sub("last_file=", "").strip if line =~ /^last_file=/
           last_speed = line.sub("last_speed=", "").strip.to_f if line =~ /^last_speed=/
           last_position = line.sub("last_position=", "").strip.to_i if line =~ /^last_position=/
-          last_speeds = line.sub("speeds=", "").strip.split(",") if line =~ /^speeds=/
-          @partial_repeat = line.sub("partial_repeat=", "").strip if line =~ /^partial_repeat=/
-          @partial_repeat = @partial_repeat == 'true' ? true : false
+          if line =~ /^speeds=/
+            last_speeds = line.sub("speeds=", "").strip.split(",")
+            last_speeds.map!{|s| s.to_f}
+          end
+          if line =~ /^partial_repeat=/
+            @partial_repeat = line.sub("partial_repeat=", "").strip
+            if @partial_repeat == 'true'
+              @partial_repeat = true
+            else
+              @partial_repeat = false
+            end
+          end
           @partial_duration = line.sub("partial_duration=", "").strip.to_i if line =~ /^partial_duration=/
         end
       end
@@ -32,10 +41,8 @@ def load_ini
 end
 
 def save_ini(file_name, speed, position, speeds)
-  position = position - 5
-  position = 0 if position < 0
   File.open(@ini_file, "w") do |f|
-    f.puts "last_file=" + file_name
+    f.puts "last_file=" + file_name.force_encoding('UTF-8')
     f.puts "last_speed=" + speed.to_s
     f.puts "last_position=" + position.to_i.to_s
     f.puts "speeds=" + speeds.join(",") if speeds != @default_speeds
@@ -44,14 +51,19 @@ def save_ini(file_name, speed, position, speeds)
   end
 end
 
-def format_seconds(seconds)
-  rewind_seconds = 5
-  seconds = seconds - rewind_seconds
+@rewind_margin = 3
+def format_startpos(seconds)
+  seconds = seconds - @rewind_margin
   seconds = 0 if seconds < 0
   hh = seconds.div(3600)
   mm = seconds.div(60) - (hh*60)
   ss = seconds % 60
   return '-ss '+hh.to_s+':'+mm.to_s+':'+ss.to_s
+end
+
+def format_endpos(seconds)
+  seconds = seconds + @rewind_margin
+  return "-endpos #{seconds}"
 end
 
 path = ARGV[0]
@@ -68,7 +80,7 @@ last_file, last_speed, last_position, last_speeds = load_ini
 if files.index(last_file) == nil
   last_file = nil
 end
-if last_speeds != nil && last_speeds != speeds
+if last_speeds != nil && last_speeds != @default_speeds
   speeds = last_speeds
 else
   speeds = @default_speeds
@@ -89,9 +101,13 @@ files.each do |f|
       duration = duration.to_i
     end
   end
+  if last_position.to_i + 5 > duration
+    last_position = nil
+    next
+  end
 
   if @partial_repeat
-    parts = duration.div(@partial_duration)
+    parts = duration.div(@partial_duration) + 1
   else
     parts = 1
   end
@@ -99,52 +115,57 @@ files.each do |f|
   parts.times do |part|
     start_pos = ''
     end_pos = ''
+
     if last_position != nil
       if @partial_repeat
-        next if last_position.div(@partial_duration * part +1).to_i > 1
+        next if last_position.div(@partial_duration).to_i != part
+        end_pos = format_endpos(@partial_duration - (last_position % @partial_duration))
       end
-      start_pos = format_seconds(last_position)
-    else
-      if @partial_repeat
-        last_position = part * @partial_duration
-        start_pos = format_seconds(part * @partial_duration)
-      end
+      start_pos = format_startpos(last_position)
     end
 
-    if @partial_repeat
-      end_pos = "-endpos " + @partial_duration.to_s
-    end
     speeds.each do |s|
+      s = s.to_f
+      if last_position == nil && @partial_repeat
+        last_position = part * @partial_duration
+        start_pos = format_startpos(part * @partial_duration)
+        end_pos = format_endpos(@partial_duration)
+      end
+
       if speeds.index(last_speed) != nil && s != last_speed
         next
       elsif last_speed == s
         last_speed = nil
       end
 
-      start_time = Time.now.to_i
-
       puts ">>> start playing #{f}".green
-      puts ">>> part #{part} of #{parts}".green if parts > 1
+      puts ">>> part #{part+1} of #{parts}".green if parts > 1
       puts ">>> with speed #{s}".green
-      result = `mplayer -nolirc -osdlevel 3 -vo x11 -af scaletempo,volnorm #{start_pos} #{end_pos} -speed #{s} -msglevel all=0 \"#{f}\"`
 
-      end_time = Time.now.to_i
-      play_seconds = (end_time - start_time) * s + last_position.to_i
+      begin
+        start_time = Time.now.to_i
+        result = `mplayer -nolirc -osdlevel 3 -vo x11 -af scaletempo,volnorm #{start_pos} #{end_pos} -speed #{s} -msglevel all=0 \"#{f}\"`
 
-      puts ">>> duration     " + duration.to_s
-      puts ">>> play seconds " + play_seconds.to_s
-
-      if @partial_repeat
-        if (end_time - start_time) * s + 5< @partial_duration
+      ensure
+        end_time = Time.now.to_i
+        play_seconds = (end_time - start_time) * s + last_position.to_i
+        if @partial_repeat
+          if (end_time - start_time) * s + 5 < @partial_duration - (last_position % @partial_duration)
+            save_ini f, s, play_seconds, speeds
+            exit
+          end
+        elsif play_seconds + 5 < duration
           save_ini f, s, play_seconds, speeds
           exit
+        else
+          start_pos = ''
         end
-      elsif play_seconds + 5 < duration
-        save_ini f, s, play_seconds, speeds
-        exit
+        puts ">>> duration     " + duration.to_s
+        puts ">>> play seconds " + play_seconds.to_s
       end
+
+      last_position = nil
     end
-    last_position = nil
 
   end
 end
